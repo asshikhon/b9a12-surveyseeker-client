0992c896b3e8829fa67b1b8263d2a0e99df8ada4
asshikhon <asshikhon@gmail.com>
2024-06-02 23:11:21 +0600

    added faq section

--- src/Pages/Home/Faq.jsx
+++ src/Pages/Home/Faq.jsx
@@ -1,10 +1,127 @@
+/* eslint-disable react/no-unescaped-entities */
+import { FaSearch } from "react-icons/fa";
+import { IoCallOutline } from "react-icons/io5";
+import { RiQuestionnaireLine } from "react-icons/ri";
 
 const Faq = () => {
-    return (
-        <div>
-            
+  return (
+<div
+
+>
+      <div className="flex container flex-col md:flex-row gap-[5%] items-center py-12 md:py-16 lg:py-20 w-[94%] md:w-full mx-auto bg-base-100 px-4 md:px-12 rounded-3xl my-12">
+        <div className="w-full">
+          <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+            Frequently Asked Questions
+          </h1>
+          <div className="flex flex-col mt-12 gap-6">
+            <div className="flex gap-6 bg-base-200 p-4 justify-center rounded-xl items-center">
+              <p className="text-7xl">
+                <RiQuestionnaireLine />
+              </p>
+              <p className="flex flex-col">
+                <span className="text-xl md:text-2xl font-semibold pb-1">
+                  Contact live chat
+                </span>
+                <span className="text-base md:text-lg">
+                  24/7 available. No chat bots
+                </span>
+              </p>
+            </div>
+            <div className="flex gap-6 bg-base-200 p-4 justify-center rounded-xl items-center">
+              <p className="text-5xl">
+                <FaSearch />
+              </p>
+              <p className="flex flex-col">
+                <span className="text-xl md:text-2xl font-semibold pb-1">
+                  Visit Help Center
+                </span>
+                <span className="text-base md:text-lg">
+                  Check out tutorials
+                </span>
+              </p>
+            </div>
+            <div className="flex gap-6 bg-base-200 p-4 justify-center rounded-xl items-center">
+              <p className="text-5xl">
+                <IoCallOutline />
+              </p>
+              <p className="flex flex-col">
+                <span className="text-xl md:text-2xl font-semibold pb-1">
+                  Call & Talk With Us
+                </span>
+                <span className="text-base md:text-lg">
+                  1:1 talk with a specialist
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-    );
+        <div>
+          <div>
+            <h3 className="text-xl md:text-3xl font-semibold text-center py-4">
+              Here are some common questions from our users.
+            </h3>
+          </div>
+          <div className="join join-vertical w-full">
+            <div className="collapse collapse-plus join-item border border-base-300">
+              <input type="radio" name="my-accordion-4" defaultChecked />
+              <div className="collapse-title text-xl md:text-3xl font-medium">
+                What is Survey?
+              </div>
+              <div className="collapse-content">
+                <p className="md:text-lg">
+                A survey is a research method used for collecting data from a predefined group of respondents to gain information and insights on various topics of interest. Surveys are commonly used in fields such as market research, social science, and public health to gather quantitative and qualitative data.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus join-item border border-base-300">
+              <input type="radio" name="my-accordion-4" />
+              <div className="collapse-title text-xl md:text-3xl font-medium">
+                How can SurveySeeker help me with my surveys?
+              </div>
+              <div className="collapse-content">
+                <p className="md:text-lg">
+                  SurveySeeker offers a range of features including customizable survey templates, real-time data analytics, and advanced reporting tools. Whether you're conducting market research, gathering customer feedback, or evaluating employee satisfaction, SurveySeeker provides the resources you need to get meaningful insights.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus join-item border border-base-300">
+              <input type="radio" name="my-accordion-4" />
+              <div className="collapse-title text-xl md:text-3xl font-medium">
+                Is SurveySeeker suitable for all types of surveys?
+              </div>
+              <div className="collapse-content">
+                <p className="md:text-lg">
+                  Yes, SurveySeeker is versatile and suitable for a variety of survey types including academic research, business evaluations, customer satisfaction, and more. Our platform is designed to cater to the needs of individuals and organizations across different industries.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus join-item border border-base-300">
+              <input type="radio" name="my-accordion-4" />
+              <div className="collapse-title text-xl md:text-3xl font-medium">
+                Are the features on SurveySeeker free to use?
+              </div>
+              <div className="collapse-content">
+                <p className="md:text-lg">
+                  SurveySeeker offers a free plan that includes basic survey creation and data analysis features. For more advanced capabilities and additional resources, we offer premium plans that provide greater flexibility and functionality.
+                </p>
+              </div>
+            </div>
+            <div className="collapse collapse-plus join-item border border-base-300">
+              <input type="radio" name="my-accordion-4" />
+              <div className="collapse-title text-xl md:text-3xl font-medium">
+                How can I contribute to SurveySeeker?
+              </div>
+              <div className="collapse-content">
+                <p className="md:text-lg">
+                  SurveySeeker welcomes contributions from survey experts, researchers, and enthusiasts. If you have valuable insights, templates, or resources to share, please reach out to our team. Together, we can enhance the platform and provide even better tools for our community.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Faq;
--- src/Pages/Home/HowItWorks.jsx
+++ src/Pages/Home/HowItWorks.jsx
@@ -1,9 +1,8 @@
 /* eslint-disable react/no-unescaped-entities */
 const HowItWorks = () => {
   return (
-    <section className="bg-gray-800 dark:bg-gray-100 text-gray-100 dark:text-gray-800">
+    <section className="">
       <div className="container flex flex-col justify-center p-4 mx-auto md:p-8">
-
         <h2 className="mb-12 text-4xl font-bold leading-none text-center sm:text-5xl">
           How It Works
         </h2>
@@ -11,37 +10,51 @@ const HowItWorks = () => {
           <div>
             <h3 className="font-semibold">Step 1: Create Your Survey</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              Start by signing up and creating an account. Once you're logged in, you can easily create a new survey using our intuitive survey builder. Customize your questions, add various types of question formats, and set up the logic that suits your needs.
+              Start by signing up and creating an account. Once you're logged
+              in, you can easily create a new survey using our intuitive survey
+              builder. Customize your questions, add various types of question
+              formats, and set up the logic that suits your needs.
             </p>
           </div>
           <div>
             <h3 className="font-semibold">Step 2: Distribute Your Survey</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              Share your survey with your target audience. SurveySeeker offers multiple distribution options including email invitations, social media sharing, and direct links. Reach your respondents wherever they are.
+              Share your survey with your target audience. SurveySeeker offers
+              multiple distribution options including email invitations, social
+              media sharing, and direct links. Reach your respondents wherever
+              they are.
             </p>
           </div>
           <div>
             <h3 className="font-semibold">Step 3: Collect Responses</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              As responses come in, you can monitor the progress in real-time. Our platform ensures secure and anonymous data collection, giving you confidence in the integrity of the results.
+              As responses come in, you can monitor the progress in real-time.
+              Our platform ensures secure and anonymous data collection, giving
+              you confidence in the integrity of the results.
             </p>
           </div>
           <div>
             <h3 className="font-semibold">Step 4: Categorize Responses</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              Organize the responses based on predefined criteria or tags. Categorization helps in better understanding and analyzing the data by grouping similar responses together.
+              Organize the responses based on predefined criteria or tags.
+              Categorization helps in better understanding and analyzing the
+              data by grouping similar responses together.
             </p>
           </div>
           <div>
             <h3 className="font-semibold">Step 5: Enable Voting</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              Set up voting mechanisms where respondents can vote on different survey questions or options. This interactive feature can provide additional insights and engagement.
+              Set up voting mechanisms where respondents can vote on different
+              survey questions or options. This interactive feature can provide
+              additional insights and engagement.
             </p>
           </div>
           <div>
             <h3 className="font-semibold">Step 6: Analyze Results</h3>
             <p className="mt-1 text-gray-400 dark:text-gray-600">
-              Use our powerful analytics tools to gain insights from your data. Visualize the results with charts and graphs, filter responses, and generate reports. Make data-driven decisions with ease.
+              Use our powerful analytics tools to gain insights from your data.
+              Visualize the results with charts and graphs, filter responses,
+              and generate reports. Make data-driven decisions with ease.
             </p>
           </div>
         </div>
