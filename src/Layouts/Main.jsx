9318603cde90e80838283c019e64c117ffd89bf9
asshikhon <asshikhon@gmail.com>
2024-06-02 19:10:25 +0600

    added footer section

--- src/Layouts/Main.jsx
+++ src/Layouts/Main.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shared/Footer/Footer";
 
 const Main = () => {
   return (
     <div>
 
 
-      <div className="min-h-[calc(100vh-373px)]">
+      <div className="min-h-[calc(100vh-273px)]">
         <Outlet></Outlet>
       </div>
 
-      
+      <Footer />
     </div>
   );
 };
